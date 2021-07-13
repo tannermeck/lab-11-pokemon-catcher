9983a316ddfda5e4602ae8b6de7bb185cedc33f6
TANNER MECK <steve.meck246@gmail.com>
2021-07-13 13:42:07 -0700

    css styling added, images created

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 // import functions
-
+import pokemonArray from './data/pokemon.js';
+console.log(pokemonArray[1]);
 // reference needed DOM elements
-
+const radio1 = document.getElementById('radio1');
+const radio2 = document.getElementById('radio2');
+const radio3 = document.getElementById('radio3');
+const submitBtn = document.getElementById('submit-button');
 
 // set event listeners 
   // get user input(s)
