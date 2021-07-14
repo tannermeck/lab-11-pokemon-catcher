f1c7f9727c512d01de2fbabb28367516c9f82f0b
TANNER MECK <steve.meck246@gmail.com>
2021-07-14 14:09:51 -0700

    setPokedex test passing

--- test/storage-utils.tests.js
+++ test/storage-utils.tests.js
@@ -1,5 +1,5 @@
 const test = QUnit.test;
-import { encounterPokemon } from '../storage-utils.js';
+import { encounterPokemon, getPokedex, setPokedex } from '../storage-utils.js';
 // name your test by what it is testing
 test('when pokemon is selected, shown increases', (expect) => {
     localStorage.removeItem('POKEMONGO');
@@ -9,9 +9,8 @@ test('when pokemon is selected, shown increases', (expect) => {
         preferred: 0
     };
     encounterPokemon(1);
-    const fakeString = localStorage.getItem('POKEMONGO') || '[]';
-    const results = JSON.parse(fakeString);
-    
+    const results = getPokedex();
+   
     expect.deepEqual(results[0], fakePokemon);
 });
 test('pokemon already selected, shown increases', (expect) => {
@@ -20,12 +19,11 @@ test('pokemon already selected, shown increases', (expect) => {
         shown: 1,
         preferred: 0
     }];
-    localStorage.setItem('POKEMONGO', JSON.stringify(fakePokemon));
+    setPokedex(fakePokemon);
     
     encounterPokemon(1);
     
-    const fakeString = localStorage.getItem('POKEMONGO') || '[]';
-    const results = JSON.parse(fakeString);
+    const results = getPokedex();
     
     const expected = {
         id: 1,
@@ -35,3 +33,38 @@ test('pokemon already selected, shown increases', (expect) => {
     
     expect.deepEqual(results[0], expected);
 });
+test('remove items from local storage and parse them with getPokedex', (expect) => {
+    const fakePokemon = [{
+        id: 1,
+        shown: 1,
+        preferred: 0
+    }];
+    setPokedex(fakePokemon);
+    
+    const expected = getPokedex();
+    
+    expect.deepEqual(expected, fakePokemon);
+});
+test('getPokedex should return an empty object if the result does not exist', (expect) => {
+    const fakePokemon = [{
+    }];
+    setPokedex(fakePokemon);
+    
+    const expected = getPokedex();
+    
+    expect.deepEqual(expected, fakePokemon);
+});
+test('setPokedex should place an object in local storage', (expect) => {
+    const fakePokemon = {
+        id: 1,
+        shown: 1,
+        preferred: 0
+    };
+    const string = JSON.stringify(fakePokemon);
+
+    const expected = '{"id":1,"shown":1,"preferred":0}';
+        
+    
+    expect.deepEqual(expected, string);
+});
+
